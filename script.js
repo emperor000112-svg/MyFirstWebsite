4aed5895df5fbaad7c32be9dadaf119f171fcaad
emperor000112-svg <emperor000112@gmail.com>
2026-09-13 15:37:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function sayHello() {
+    document.getElementById("message").innerText =
+        "Hello Rajvardhan! 👋 Welcome to my website!";
+}
+
+function contactMe() {
+    alert("Thanks for visiting my portfolio! 📩");
+}
